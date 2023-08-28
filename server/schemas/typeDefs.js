877fced10fc60e7d191c0655e72c8e06a1a489e1
Peter Conenna <pconenna@depaul.edu>
2023-08-28 15:29:33 -0500

    finished typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,7 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type Book {
+        BookId: String
         authors: [String]
         description: String
         image: String
@@ -13,12 +14,32 @@ const typeDefs = gql`
         username: String
         email: String
         password: String
+        bookCount: Int
         savedBooks: [Book]
+    }
+    Type Auth{
+        token: ID!
+        user: User
+    }
+    input BookInput{
+        BookId: String
+        description: String
+        image: String
+        link: String
+        title: String
     }
      type Query {
         books: [Book]
         book (title: String!): Book
         users: [User]
         user(username: String!): User
+        me: User
+     }
+
+     type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [Book]!, input: bookInput): User
+        removeBook(bookId: String!):User
      }
 `
